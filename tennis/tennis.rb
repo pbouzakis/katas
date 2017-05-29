f9e95af2f866d3c7a9c9e2c22d83d3ad80d97963
Paul Bouzakis <github@paulbouzakis.com>
2017-05-29 13:30:43 -0400

    Start new kata 'Tennis', with incomplete Ruby impl.

---
+++ tennis/tennis.rb
@@ -0,0 +1,176 @@
+require 'minitest/autorun'
+require "minitest/spec"
+
+class TennisGame
+    attr_accessor :winner
+
+    def initialize
+        initial_points = Points.create
+        @player_one = Player.new(:player_one, initial_points)
+        @player_two = Player.new(:player_two, initial_points)
+        @winner = nil
+    end
+
+    def score
+        Score.new(@player_one.points, @player_two.points)
+    end
+
+    def player_one_scores
+        @player_one.score_against(@player_two)
+        check_if_winner(@player_one)
+    end
+
+    def player_two_scores
+        @player_two.score_against(@player_one)
+        check_if_winner(@player_two)
+    end
+
+    def over?
+        @winner != nil
+    end
+
+    def check_if_winner(player)
+        if player.won?
+            @winner = player
+        end
+    end
+end
+
+Score = Struct.new(:player_one, :player_two) do
+end
+
+class Player
+    attr_accessor :name
+
+    def initialize(name, points)
+        @name = name
+        @points = points
+        @won = false
+    end
+
+    def points
+        @points.value
+    end
+
+    def score_against(opponent)
+        if points == Points::FORTY && opponent.points < Points::FORTY
+            @won = true
+        else
+            @points = @points.next_point
+        end
+    end
+
+    def won?
+        @won
+    end
+end
+
+class Points
+    attr_accessor :value, :next_point
+
+    LOVE = 0
+    FIFTEEN = 15
+    THIRTY = 30
+    FORTY = 40
+
+    def initialize(value, next_point)
+        @value = value
+        @next_point = next_point
+    end
+
+    def self.create
+        Points.new(Points::LOVE,
+            Points.new(Points::FIFTEEN,
+                Points.new(Points::THIRTY,
+                    Points.new(Points::FORTY, nil))))
+    end
+end
+
+describe TennisGame do
+    before do
+        @game = TennisGame.new
+    end
+
+    describe "when the game starts" do
+        it "has both players at LOVE" do
+            @game.score.must_equal Score.new(Points::LOVE, Points::LOVE)
+        end
+    end
+
+    describe "when player one scores first" do
+        before do
+            @game.player_one_scores
+        end
+
+        it "has the score set to FIFTEEN LOVE" do
+            @game.score.must_equal Score.new(Points::FIFTEEN, Points::LOVE)
+        end
+    end
+
+    describe "when player two scores first" do
+        before do
+            @game.player_two_scores
+        end
+
+        it "has the score set to LOVE FIFTEEN" do
+            @game.score.must_equal Score.new(Points::LOVE, Points::FIFTEEN)
+        end
+    end
+
+    describe "when a player scores the first 2 times " do
+        before do
+            @game.player_one_scores
+            @game.player_one_scores
+        end
+
+        it "has the score set to THIRTY LOVE" do
+            @game.score.must_equal Score.new(Points::THIRTY, Points::LOVE)
+        end
+    end
+
+    describe "when a player scores the first 3 times " do
+        before do
+            @game.player_one_scores
+            @game.player_one_scores
+            @game.player_one_scores
+        end
+
+        it "has the score set to THIRTY LOVE" do
+            @game.score.must_equal Score.new(Points::FORTY, Points::LOVE)
+        end
+    end
+
+    describe "when player one scores the first 4 times " do
+        before do
+            @game.player_one_scores
+            @game.player_one_scores
+            @game.player_one_scores
+            @game.player_one_scores
+        end
+
+        it "has the game over" do
+            @game.over?.must_equal true
+        end
+
+        it "has the player one as the winner" do
+            @game.winner.name.must_equal :player_one
+        end
+    end
+
+    describe "when a player two scores the first 4 times " do
+        before do
+            @game.player_two_scores
+            @game.player_two_scores
+            @game.player_two_scores
+            @game.player_two_scores
+        end
+
+        it "has the game over" do
+            @game.over?.must_equal true
+        end
+
+        it "has the player one as the winner" do
+            @game.winner.name.must_equal :player_two
+        end
+    end
+end
